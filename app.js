ecba97d8cf4a9ab165f256d9690a85662d28de20
Vidar Kodehode <125680728+VidarHeritier@users.noreply.github.com>
2023-09-20 15:25:09 +0200

    Now all pages functions as intended

--- app.js
+++ app.js
@@ -20,6 +20,7 @@ const menuItem2 = document.querySelector(".menu-item2");
 const menuItem3 = document.querySelector(".menu-item3");
 
 let isHamburgerOpen = false;
+let isPageLoaded = false;
 
 /**Hamburger menu hover anim */
 hiddenBtn.addEventListener("mouseenter", (event) => {
@@ -133,9 +134,9 @@ menuItem3.addEventListener("click", (event) => {
     menuItem1.style.visibility = "hidden";
     welcome.remove();
 
-    // setTimeout(() => {
-    //   populateAboutPage();
-    // }, 150);
+    setTimeout(() => {
+      populateContactPage();
+    }, 150);
 
     isHamburgerOpen = false;
 
@@ -147,109 +148,138 @@ menuItem3.addEventListener("click", (event) => {
 
 /**Populating the projects page */
 const populateProjectsPage = () => {
-  const projectsDiv = document.createElement("div");
-  projectsDiv.classList.add("projects");
+  deleteChild();
+  if (isPageLoaded === false) {
+    const projectsDiv = document.createElement("div");
+    projectsDiv.classList.add("projects");
 
-  /**Making arrow components */
-  const leftArrow = document.createElement("div");
-  leftArrow.classList.add("left-arrow");
-  const leftArrowStyle = document.createElement("div");
-  leftArrowStyle.classList.add("left-arrow-style");
-  leftArrow.append(leftArrowStyle);
-  const rightArrow = document.createElement("div");
-  rightArrow.classList.add("right-arrow");
-  const rightArrowStyle = document.createElement("div");
-  rightArrowStyle.classList.add("right-arrow-style");
-  rightArrow.append(rightArrowStyle);
+    /**Making arrow components */
+    const leftArrow = document.createElement("div");
+    leftArrow.classList.add("left-arrow");
+    const leftArrowStyle = document.createElement("div");
+    leftArrowStyle.classList.add("left-arrow-style");
+    leftArrow.append(leftArrowStyle);
+    const rightArrow = document.createElement("div");
+    rightArrow.classList.add("right-arrow");
+    const rightArrowStyle = document.createElement("div");
+    rightArrowStyle.classList.add("right-arrow-style");
+    rightArrow.append(rightArrowStyle);
 
-  /**Project 1 creation */
-  const project1 = document.createElement("div");
-  project1.classList.add("project1");
-  project1.classList.add("project-elements");
-  const projectTitle = document.createElement("h3");
-  projectTitle.textContent = "Projects";
-  const project1IMG = new Image();
-  project1IMG.src =
-    "./Projects/5ffb88178839407.Y3JvcCwzMzYwLDI2MjgsMCwzNjU.jpg";
-  const project1Text = document.createElement("h4");
-  project1Text.textContent = "lorem ipsum1";
-  project1.append(project1IMG, project1Text);
+    /**Project 1 creation */
+    const project1 = document.createElement("div");
+    project1.classList.add("project1");
+    project1.classList.add("project-elements");
+    const projectTitle = document.createElement("h3");
+    projectTitle.textContent = "Projects";
+    const project1IMG = new Image();
+    project1IMG.src =
+      "./Projects/5ffb88178839407.Y3JvcCwzMzYwLDI2MjgsMCwzNjU.jpg";
+    const project1Text = document.createElement("h4");
+    project1Text.textContent = "lorem ipsum1";
+    project1.append(project1IMG, project1Text);
 
-  /**Project 2 creation */
-  const project2 = document.createElement("div");
-  project2.classList.add("project2");
-  project2.classList.add("project-elements");
-  const project2IMG = new Image();
-  project2IMG.src =
-    "./Projects/8b3317178642685.Y3JvcCw1NTIzLDQzMjAsMTgzMSww.png";
-  const project2Text = document.createElement("h4");
-  project2Text.textContent = "lorem ipsum2";
-  project2.append(project2IMG, project2Text);
+    /**Project 2 creation */
+    const project2 = document.createElement("div");
+    project2.classList.add("project2");
+    project2.classList.add("project-elements");
+    const project2IMG = new Image();
+    project2IMG.src =
+      "./Projects/8b3317178642685.Y3JvcCw1NTIzLDQzMjAsMTgzMSww.png";
+    const project2Text = document.createElement("h4");
+    project2Text.textContent = "lorem ipsum2";
+    project2.append(project2IMG, project2Text);
 
-  /**Project 3 creation */
-  const project3 = document.createElement("div");
-  project3.classList.add("project3");
-  project3.classList.add("project-elements");
-  const project3IMG = new Image();
-  project3IMG.src =
-    "./Projects/depositphotos_403126704-stock-illustration-architecture-project-showing-different-design.jpg";
-  const project3Text = document.createElement("h4");
-  project3Text.textContent = "lorem ipsum3";
-  project3.append(project3IMG, project3Text);
+    /**Project 3 creation */
+    const project3 = document.createElement("div");
+    project3.classList.add("project3");
+    project3.classList.add("project-elements");
+    const project3IMG = new Image();
+    project3IMG.src =
+      "./Projects/depositphotos_403126704-stock-illustration-architecture-project-showing-different-design.jpg";
+    const project3Text = document.createElement("h4");
+    project3Text.textContent = "lorem ipsum3";
+    project3.append(project3IMG, project3Text);
 
-  /**Project 4 creation */
-  const project4 = document.createElement("div");
-  project4.classList.add("project4");
-  project4.classList.add("project-elements");
-  const project4IMG = new Image();
-  project4IMG.src = "./Projects/img-issues-incidents-management-process.png";
-  const project4Text = document.createElement("h4");
-  project4Text.textContent = "lorem ipsum4";
-  project4.append(project4IMG, project4Text);
+    /**Project 4 creation */
+    const project4 = document.createElement("div");
+    project4.classList.add("project4");
+    project4.classList.add("project-elements");
+    const project4IMG = new Image();
+    project4IMG.src = "./Projects/img-issues-incidents-management-process.png";
+    const project4Text = document.createElement("h4");
+    project4Text.textContent = "lorem ipsum4";
+    project4.append(project4IMG, project4Text);
 
-  /**Project 5 creation */
-  const project5 = document.createElement("div");
-  project5.classList.add("project5");
-  project5.classList.add("project-elements");
-  const project5IMG = new Image();
-  project5IMG.src = "./Projects/IMG_3151-1024x1024.jpg";
-  const project5Text = document.createElement("h4");
-  project5Text.textContent = "lorem ipsum5";
-  project5.append(project5IMG, project5Text);
+    /**Project 5 creation */
+    const project5 = document.createElement("div");
+    project5.classList.add("project5");
+    project5.classList.add("project-elements");
+    const project5IMG = new Image();
+    project5IMG.src = "./Projects/IMG_3151-1024x1024.jpg";
+    const project5Text = document.createElement("h4");
+    project5Text.textContent = "lorem ipsum5";
+    project5.append(project5IMG, project5Text);
 
-  const projectPage = document.createElement("div");
-  projectPage.classList.add("project-page");
-  projectPage.style.animation = "expandMenuItem2 1.5s forwards";
+    const projectPage = document.createElement("div");
+    projectPage.classList.add("project-page");
+    projectPage.style.animation = "expandMenuItem2 1.5s forwards";
 
-  /**All elements in main div */
-  const projectHeaderDiv = document.createElement("div");
-  projectHeaderDiv.classList.add("project-header");
-  const projectHeader = document.createElement("h3");
-  projectHeader.textContent = "Projects";
-  projectHeaderDiv.style.animation = "moveProjectHeader 1s forwards";
-  projectHeaderDiv.append(projectHeader);
+    /**All elements in main div */
+    const projectHeaderDiv = document.createElement("div");
+    projectHeaderDiv.classList.add("project-header");
+    const projectHeader = document.createElement("h3");
+    projectHeader.textContent = "Projects";
+    projectHeaderDiv.style.animation = "moveProjectHeader 1s forwards";
+    projectHeaderDiv.append(projectHeader);
 
-  projectsDiv.append(
-    leftArrow,
-    project1,
-    project2,
-    project3,
-    project4,
-    project5,
-    rightArrow
-  );
-  projectPage.append(projectsDiv);
-  main.append(projectPage);
-  navbar.append(projectHeaderDiv);
+    const menuBar = document.querySelector(".menu-bar");
+    projectsDiv.append(project1, project2, project3, project4, project5);
+    projectPage.append(leftArrow, projectsDiv, rightArrow);
+    main.append(projectPage);
+    menuBar.append(projectHeaderDiv);
+
+    /**Mouse over functionality for the carousel */
+    const stepWidth = 10;
+    function moveCarouselLeft() {
+      const currentWidth = Number(
+        projectsDiv.style.transform.replace("vw", "")
+      );
+      projectsDiv.style.transform = `translateX(${currentWidth - stepWidth}vw)`;
+    }
+
+    // Add an event listener to the left arrow (or any other trigger)
+    leftArrow.addEventListener("click", (event) => {
+      moveCarouselLeft();
+    });
+    rightArrow.addEventListener("click", (event) => {
+      projectsDiv.style.transform = "translateX(-5vw)";
+    });
+  }
+  isPageLoaded = true;
 };
 
-/**Populating the projects page */
+/**Populating the about page */
 const populateAboutPage = () => {
+  deleteChild();
+
   const aboutDiv = document.createElement("div");
   aboutDiv.classList.add("about");
   const aboutText = document.createElement("p");
   aboutText.textContent = "lorem20 lorem20 lorem20 lorem20";
   aboutDiv.append(aboutText);
+  main.append(aboutDiv);
+};
+
+/**Populating the contact page */
+const populateContactPage = () => {
+  deleteChild();
+
+  const contactDiv = document.createElement("div");
+  contactDiv.classList.add("contact");
+  const contactText = document.createElement("p");
+  contactText.textContent = "Contact me pls";
+  contactDiv.append(contactText);
+  main.append(contactDiv);
 };
 
 /**Recreate hamburger menu */
@@ -271,3 +301,14 @@ const recreateHamburgerMenu = () => {
   hamburgerItem2.style.animation = "backIn2 .6s forwards";
   hamburgerItem3.style.animation = "backIn .6s forwards";
 };
+
+/**Delete Pages */
+function deleteChild() {
+  const e = document.querySelector(".main");
+
+  var child = e.lastElementChild;
+  while (child) {
+    e.removeChild(child);
+    child = e.lastElementChild;
+  }
+}
