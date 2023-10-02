809c23e25170517460e6b63762a806a32f6a3f12
Vidar Kodehode <125680728+VidarHeritier@users.noreply.github.com>
2023-10-02 12:44:49 +0200

    try

--- app.js
+++ app.js
@@ -196,24 +196,17 @@ window.addEventListener("touchend", function (e) {
   const deltaY = Math.abs(e.changedTouches[0].clientY - initialY);
   const deltaTime = new Date() - initialTime;
 
-  console.log("DeltaX:", deltaX);
-  console.log("DeltaY:", deltaY);
-  console.log("DeltaTime:", deltaTime);
 
-  if (deltaX <= -30) {
-    swipeDirection = -1;
-  } else if (deltaX >= 30) {
-    swipeDirection = 1;
-  }
+  if (deltaX <= -30 || deltaX >= 30) {
+  
+  swipeDirection = deltaX <= -30 ? -1 : 1;
 
-  if (Math.abs(deltaX) >= 30 && deltaY <= 100 && deltaTime <= 300) {
-    isSwipe = true;
+  isSwipe = true;
 
-    setTimeout(() => {
-      isSwipe = false;
-    }, 500);
-  }
-});
+  setTimeout(() => {
+    isSwipe = false;
+  }, 500);
+}
 /**Arrows */
 document.addEventListener("keydown", (event) => {
   if (event.key === "ArrowUp" || event.key === "ArrowDown") {
